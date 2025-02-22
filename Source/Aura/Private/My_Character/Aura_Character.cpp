9bce069b68a57dca7fea5d19ea7ba18d941e1a52
Pcr <794676750@qq.com>
2025-02-23 00:24:21 +0800

    补充上面

--- Source/Aura/Private/My_Character/Aura_Character.cpp
+++ Source/Aura/Private/My_Character/Aura_Character.cpp
@@ -11,10 +11,8 @@ AAura_Character::AAura_Character()
 
     // 角色在移动时自动朝向移动方向
     GetCharacterMovement()->bOrientRotationToMovement = true;
-
     // Yaw 左右转向
     GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // 每秒旋转 540 度
-
     // 禁用平面约束（测试时可以先禁用）
     GetCharacterMovement()->bConstrainToPlane = false;
     GetCharacterMovement()->bSnapToPlaneAtStart = false;
