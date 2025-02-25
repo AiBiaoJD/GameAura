fb71f5172ed7b6717622e48389f4307bbb2bfc53
Pcr <794676750@qq.com>
2025-02-25 16:15:14 +0800

    学习了Attribute属性的创建并设置为replica
    1.FGMEATTitudeDAte是基本的ATTribute类的数据类型，包括base 和 cur两个变量
    
    2.创建属性方法
    (1)UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Health, Category= "Health")
    FGameplayAttributeData Health;
    设置为replicated并调用回调函数
    
    (2)定义回调函数
    	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
    
    （3）实现回调函数
    void UMy_AuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
    {
    	//宏，比较旧值和新值，不同调用OnRep，上面GetLifetimeReplicatedProps改了相同也调用
    	GAMEPLAYATTRIBUTE_REPNOTIFY(UMy_AuraAttributeSet, Health, OldHealth);
    
    }
    
    (4)更新属性到GetLifetimeReplicatedProps（）
    	DOREPLIFETIME_CONDITION_NOTIFY(UMy_AuraAttributeSet, Health, COND_None, REPNOTIFY_Always);

--- Source/Aura/Private/MY_AbilitySystem/My_AuraAttributeSet.cpp
+++ Source/Aura/Private/MY_AbilitySystem/My_AuraAttributeSet.cpp
@@ -3,3 +3,42 @@
 
 #include "MY_AbilitySystem/My_AuraAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UMy_AuraAttributeSet::UMy_AuraAttributeSet()
+{
+
+}
+
+void UMy_AuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//Replicate 无条件的复制，总是有调用OnRep，即时相同
+	DOREPLIFETIME_CONDITION_NOTIFY(UMy_AuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMy_AuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMy_AuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMy_AuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UMy_AuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	//宏，比较旧值和新值，不同调用OnRep，上面GetLifetimeReplicatedProps改了相同也调用
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMy_AuraAttributeSet, Health, OldHealth);
+
+}
+void UMy_AuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMy_AuraAttributeSet, Health, OldMaxHealth);
+}
+
+void UMy_AuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMy_AuraAttributeSet, Mana, OldMana);
+}
+
+void UMy_AuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMy_AuraAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/Aura/Public/MY_AbilitySystem/My_AuraAttributeSet.h
+++ Source/Aura/Public/MY_AbilitySystem/My_AuraAttributeSet.h
@@ -7,11 +7,46 @@
 #include "My_AuraAttributeSet.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class AURA_API UMy_AuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+
+	UMy_AuraAttributeSet();
+	/*
+	1.定义哪些属性需要在网络上复制
+	2.每个属性设置特定的复制条件和规则
+	3.通过精确控制需要复制的属性，可以优化网络带宽使用
+	*/
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//ReplicatedUsing = OnRep_Health,属性更新时执行回调函数
+	//血量
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Health, Category= "Health")
+	FGameplayAttributeData Health;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "MaxHealth")
+	FGameplayAttributeData MaxHealth;
+
+	//蓝量
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Mana, Category="Mana")
+	FGameplayAttributeData Mana;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxMana, Category= "MaxMana")
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
 };
+
