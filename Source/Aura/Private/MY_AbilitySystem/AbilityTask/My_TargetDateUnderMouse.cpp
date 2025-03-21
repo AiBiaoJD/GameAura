ce3b2f183102f84c8d0cc5e4dbb2d0f5d4ff019d
Pcr <794676750@qq.com>
2025-03-22 05:04:40 +0800

    总结：Ability下面的AbilityTask 允许开发者将需要时间或依赖于外部事件的任务封装成可重用的任务对象，从而简化 GameplayAbility 的实现。
    1. AbilityTask 的作用
    
    1.1异步操作：
    
    AbilityTask 允许 GameplayAbility 执行需要时间的操作（如等待输入、延迟执行、动画播放等），而不会阻塞 Ability 的执行。
    
    1.2事件驱动：
    
    AbilityTask 可以监听外部事件（如输入事件、游戏事件、动画通知等），并在事件触发时执行相应的逻辑。
    
    1.3模块化设计：
    
    通过将复杂的逻辑封装到 AbilityTask 中，可以使 GameplayAbility 的代码更加简洁和模块化。
    
    1.4任务管理：
    
    AbilityTask 可以管理任务的启动、执行和结束，确保任务的生命周期与 Ability 的生命周期一致。
    
    2.AbilityTask 的主要类型
    
    2.1WaitDelay：
    
    延迟一段时间后继续执行。
    
    示例：在技能释放后延迟一段时间再执行后续逻辑。
    
    2.2WaitInputPress：
    
    等待玩家按下某个输入按键。
    
    示例：等待玩家按下攻击键后触发技能。
    
    2.3PlayMontageAndWait：
    
    播放动画蒙太奇并等待其完成。
    
    示例：播放攻击动画并在动画完成后执行伤害计算。
    
    2.4WaitGameplayEvent：
    
    等待特定的 GameplayEvent 触发。
    
    示例：等待技能命中事件触发后执行伤害计算。
    
    2.5WaitTargetData：
    
    等待目标选择数据（如鼠标点击或区域选择）。
    
    示例：等待玩家选择一个目标后释放技能。
    
    2.6WaitAttributeChange：
    
    等待角色属性发生变化。
    
    示例：等待角色的生命值低于一定值时触发技能。
    
    2.7WaitCancel：
    
    等待 Ability 被取消。
    
    示例：在技能释放过程中监听取消事件。
    
    2.8WaitAbilityCommit：
    
    等待另一个 Ability 提交（Commit）。
    
    示例：在技能释放过程中等待另一个技能完成。

---
+++ Source/Aura/Private/MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.cpp
@@ -0,0 +1,5 @@
+// Copyright ABiao
+
+
+#include "MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.h"
+
---
+++ Source/Aura/Public/MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.h
@@ -0,0 +1,17 @@
+// Copyright ABiao
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "My_TargetDateUnderMouse.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UMy_TargetDateUnderMouse : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+};
