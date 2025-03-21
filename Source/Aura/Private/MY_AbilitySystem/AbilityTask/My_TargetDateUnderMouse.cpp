38e2acc16b5dc6f3c02cdc61bc4e0ed0174f4662
Pcr <794676750@qq.com>
2025-03-22 06:35:56 +0800

    总结：创建了自己的AbilityTask。
    1.Task仿照UAbilityTask_PlayMontageAndWait实现。
    其中函数来创建自己的实例，可以在蓝图中调用，这样就可以使用Task,有点像单例模式
    
    2.Task里面主要是使用委托来作为蓝图中的输出引脚
    其中委托和委托的值都会作为引脚使用下面方式.
    DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMy_OnMouseTargetDateSignature, const FVector&, Data);
    UPROPERTY(BlueprintAssignable)
    FMy_OnMouseTargetDateSignature ValidData;
    
    会有一个Fvector和委托输出
    
    3.Activate()是Task创建时ASC自动调用，因此可以在这里使用委托进行广播CursorLocation,这样在蓝图中委托就是绑定事件的，并且value是广播的值，可以用来处理相应事情。
    
    4.问题：
    服务器和客户端在自己本地都可以使用这个委托来显示球体。但是客户端运行Ability,服务器会显示球体，但是位置是0,0,0。表示服务器找不到鼠标位置？

--- Source/Aura/Private/MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.cpp
+++ Source/Aura/Private/MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.cpp
@@ -2,4 +2,21 @@
 
 
 #include "MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.h"
+#include "My_Controler/My_Aura_Controller.h"
 
+UMy_TargetDateUnderMouse* UMy_TargetDateUnderMouse::CreateMyTargetDateUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UMy_TargetDateUnderMouse* MyObj = NewAbilityTask<UMy_TargetDateUnderMouse>(OwningAbility);
+
+
+	return  MyObj;
+}
+
+void UMy_TargetDateUnderMouse::Activate()
+{
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHit;
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	ValidData.Broadcast(CursorHit.Location);
+
+}
--- Source/Aura/Public/MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.h
+++ Source/Aura/Public/MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.h
@@ -6,6 +6,7 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "My_TargetDateUnderMouse.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMy_OnMouseTargetDateSignature, const FVector&, Data);
 /**
  * 
  */
@@ -13,5 +14,23 @@ UCLASS()
 class AURA_API UMy_TargetDateUnderMouse : public UAbilityTask
 {
 	GENERATED_BODY()
-	
+
+public:
+	/*
+	 * 仿照UAbilityTask_PlayMontageAndWait,实现自己的Task
+	 * Task要用函数返回自己的实例Instance
+	 */
+	UFUNCTION(BlueprintCallable, Category="My_Ability|Tasks", meta=(DisplayName = "MyTargetDateUnderMouse", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "true"))
+	static UMy_TargetDateUnderMouse* CreateMyTargetDateUnderMouse(UGameplayAbility* OwningAbility);
+
+	/*
+	 * 委托作为蓝图中上面函数的输出引脚
+	 */
+	UPROPERTY(BlueprintAssignable)
+	FMy_OnMouseTargetDateSignature ValidData;
+
+private:
+	// Task创建ASC自动调用下面的函数
+	virtual void Activate() override;
+
 };
