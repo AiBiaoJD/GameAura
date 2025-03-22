ea026cb2b51ee40a2202d7b36087cad877341c6a
Pcr <794676750@qq.com>
2025-03-22 15:41:01 +0800

    总结：我们使用自定义的UnderMouse_Task来处理火球Ability中获取鼠标位置信息。
    1.我们是在Task的Activate中直接通过HitResult获取相关鼠标信息，但是会造成客户端触发Ability调用Task，服务器没有接受到HitResult，服务器不显示鼠标信息。因为我们没有传入鼠标信息给服务器，而Activate自动传入服务器但是没数据，所以没有信息。
    
    2.怎么传入HitResult给服务器呢？
    根据前面两个总结，我们采用TargetData方式 传给服务器，FGameplayAbilityTargetData_SingleTargetHit，里面包含了鼠标点击的HitResult。
    
    3.在Task的Activate中，我们区分本地和不是本地，本地可能是服务器或者客户端，不是本地只能是服务器。
    
    3.1在本地中，我们使用ServerSetReplicatedTargetData()函数传入TargetData给服务器，其实这里是进行使用委托进行广播TargetData。
    之后使用Task中自定义的委托(在蓝图中为输出引脚),进行广播TargetData给蓝图，这样就本地可以使用了。
    
    3.2在服务器中,我们会使用
    (1)AbilityTargetDataSetDelegate()当接受到一个TargetData,会使用委托进行绑定相关函数，因为上面传入TargetData是广播委托，而这里我们需要绑定委托来接受TargetData。
    
    绑定委托的回调函数就是服务器上调用Task中自定义的委托(在蓝图中为输出引脚),进行广播TargetData给蓝图，这样服务器也可以显示了。
    
    (2)我们这里广播委托可能会发生在绑定委托前面，具体分析看前面2个总结，服务器Activate可能后执行，而TargetData可能先执行广播。
    
    因此要使用bool CallReplicatedTargetDataDelegatesIfSet()来重新广播委托，直到返回TargetData，因为先广播后绑定收不到TargetData。
    
    如果函数返回false，说明没有收到TargetData，使用SetWaitingOnRemotePlayerData();等待。
    
    4.UMy_AuraAssetManager需要使用下面代码，来启动TargetData
    // This is Required to Use TargetData
    UAbilitySystemGlobals::Get().InitGlobalData();

--- Source/Aura/Private/MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.cpp
+++ Source/Aura/Private/MY_AbilitySystem/AbilityTask/My_TargetDateUnderMouse.cpp
@@ -13,13 +13,27 @@ UMy_TargetDateUnderMouse* UMy_TargetDateUnderMouse::CreateMyTargetDateUnderMouse
 void UMy_TargetDateUnderMouse::Activate()
 {
 	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+
+	// 本地
 	if (bIsLocallyControlled)
 	{
 		SendMouseCursurData();
 	}
+	// 服务器
 	else
 	{
-		//TODO:On Server Listen Data
+		AbilitySystemComponent.Get()->AbilityTargetDataSetDelegate(GetAbilitySpecHandle(), GetActivationPredictionKey()).AddUObject(this, &UMy_TargetDateUnderMouse::OnTargetDataReplicatedCallback);
+
+		/*
+		 * 服务器在上面委托绑定回调函数前,已经执行了委托的广播,错误的
+		 * 因此要再广播上面的委托,直到服务器收到TargetDate
+		 * ture表明收到TargetData;false为没收到TargetData,上面委托需要广播
+		 */
+		const bool bCalledDelegate = AbilitySystemComponent.Get()->CallReplicatedTargetDataDelegatesIfSet(GetAbilitySpecHandle(), GetActivationPredictionKey());
+		if (!bCalledDelegate)
+		{
+			SetWaitingOnRemotePlayerData();
+		}
 	}
 
 
@@ -41,14 +55,26 @@ void UMy_TargetDateUnderMouse::SendMouseCursurData()
 	// 客户端传TargetData给服务器
 	AbilitySystemComponent->ServerSetReplicatedTargetData(
 		GetAbilitySpecHandle(),
-		GetActivationPredictionKey(), 
-		DataHandle, 
+		GetActivationPredictionKey(),
+		DataHandle,
 		FGameplayTag(),
 		AbilitySystemComponent->ScopedPredictionKey);
 
-	// 服务器本地广播委托给蓝图
+	// 客户端(也可能是服务器)广播委托给蓝图
 	if (ShouldBroadcastAbilityTaskDelegates())
 	{
 		ValidData.Broadcast(DataHandle);
 	}
+}
+
+void UMy_TargetDateUnderMouse::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle,
+	FGameplayTag ActivateTag)
+{
+	// 服务器收到TargetData后,会自动存储TargetData,这里让服务器不需要再存储了
+	AbilitySystemComponent.Get()->ConsumeClientReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey());
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+
 }
