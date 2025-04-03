7bc3c1a35db47fd5be3f3057702ec4a352fe6357
Pcr <794676750@qq.com>
2025-04-03 12:34:04 +0800

    总结：实现了敌人护甲和Aura的护甲穿透对伤害计算。
    护甲穿透：就是根据百分比计算实际敌人护甲是多少
    
    护甲：根据护甲占100的多少百分比实际计算伤害，。

--- Source/Aura/Private/MY_AbilitySystem/ExeCalc/My_ExeCalc_Damage.cpp
+++ Source/Aura/Private/MY_AbilitySystem/ExeCalc/My_ExeCalc_Damage.cpp
@@ -11,10 +11,12 @@
 struct My_AuraDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 	My_AuraDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UMy_AuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UMy_AuraAttributeSet, ArmorPenetration, Source, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UMy_AuraAttributeSet, BlockChance, Target, false);
 	}
 };
@@ -26,6 +28,7 @@ static const My_AuraDamageStatics& My_DamageStatics()
 UMy_ExeCalc_Damage::UMy_ExeCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(My_DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(My_DamageStatics().ArmorPenetrationDef);
 	RelevantAttributesToCapture.Add(My_DamageStatics().BlockChanceDef);
 
 }
@@ -46,30 +49,29 @@ void UMy_ExeCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecu
 	// 获取Damage
 	float Damage = Spec.GetSetByCallerMagnitude(FMy_AuraGameplayTags::GetInstance().My_EffectData_Damage);
 
-	// 获取格挡概率（BlockChance)
-	float BlockChance = 0.f;
-	const UMy_AuraAttributeSet* My_AuraAttribute = Cast<UMy_AuraAttributeSet>(TargetASC->GetAttributeSet(UMy_AuraAttributeSet::StaticClass()));
-	if (My_AuraAttribute)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("EnemyBlockChance (Direct) = %f"), My_AuraAttribute->GetBlockChance());
-	}
-
-
-	if (!ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(My_DamageStatics().BlockChanceDef, EvaluateParameters, BlockChance))
-	{
-		UE_LOG(LogTemp, Error, TEXT("Failed to capture BlockChance!"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("BlockChance = %f"), BlockChance);
-
-	}
-	const bool isBlocked = FMath::RandRange(0.f, 1.f) <= BlockChance;
+	// 获取格挡概率(BlockChance)
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(My_DamageStatics().BlockChanceDef, EvaluateParameters, TargetBlockChance);
+	TargetBlockChance = FMath::Max(0.f, TargetBlockChance);
+	const bool isBlocked = FMath::RandRange(0.f, 1.f) <= TargetBlockChance;
 	if (isBlocked)
 	{
 		Damage *= 0.5f;
 	}
 
+	// 获取护甲(Armor)
+	float TargetArmor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(My_DamageStatics().ArmorDef, EvaluateParameters, TargetArmor);
+	TargetArmor = FMath::Max(0.f, TargetArmor);
+
+	// 获取护甲穿透(Armor)
+	float SourceArmorPenetration = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(My_DamageStatics().ArmorPenetrationDef, EvaluateParameters, SourceArmorPenetration);
+	SourceArmorPenetration = FMath::Max(0.f, SourceArmorPenetration);
+
+	// 根据护甲和护甲穿透计算Damage
+	float EffectiveArmor = (1 - SourceArmorPenetration * 0.25) * TargetArmor;
+	Damage *= (100 - EffectiveArmor * 0.25) / 100;
 
 	// 输出伤害
 	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(
