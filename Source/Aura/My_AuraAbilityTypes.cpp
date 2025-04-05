682af48120ea4fd790d9c73127d1adb8b5074c15
Pcr <794676750@qq.com>
2025-04-05 16:22:51 +0800

    总结：创建自己的EffectContext，继承FgameplayEffectContext
    其中要先使用下面函数进行初始化，再添加自己想要的变量
    GetScriptStruct()
    NetSerialize()

---
+++ Source/Aura/My_AuraAbilityTypes.cpp
@@ -0,0 +1,6 @@
+﻿#include "My_AuraAbilityTypes.h"
+
+bool FMY_AuraGamePlayEffectContext::NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess)
+{
+	return true;
+}
---
+++ Source/Aura/My_AuraAbilityTypes.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "GameplayEffectTypes.h"
+#include "My_AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FMY_AuraGamePlayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+public:
+	bool IsCriticalHit() const { return bIsCriticalHit; };
+	bool IsBlockedHit() const { return bIsBlockedHit; };
+	void SetIsCriticalHit(bool bInCriticalHit) { bIsCriticalHit = bInCriticalHit; }
+	void SetIsBlockedHit(bool bInBlockedHit) { bIsBlockedHit = bInBlockedHit; }
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+protected:
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
