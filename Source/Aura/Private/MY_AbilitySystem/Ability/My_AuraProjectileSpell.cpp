b06627b44a089c1caacda409a528f47aceb82fc9
Pcr <794676750@qq.com>
2026-06-17 03:10:21 +0800

    WIP: 修复Demon_Ranger动画蓝图，尝试UpperBody Slot边走边施法
    - 修复：Demon_Ranger未设置My_ABP_Demon导致Montage Event收不到
    - 尝试：Aura角色施法时UpperBody Slot分离（LayeredBlendPerBone，未完成）
    - My_GA_RangerAttack：恢复至哥布林远程攻击可用的版本
    - My_AM_Demon_Throw：添加My_AN_MontageEvent AnimNotify
    - 已知问题：边走边施法时，UpperBody Slot混合节点未正常工作

--- Source/Aura/Private/MY_AbilitySystem/Ability/My_AuraProjectileSpell.cpp
+++ Source/Aura/Private/MY_AbilitySystem/Ability/My_AuraProjectileSpell.cpp
@@ -15,7 +15,7 @@ void UMy_AuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle H
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
-void UMy_AuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation)
+void UMy_AuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag)
 {
 	// ProjectileActor需要在服务器生成,replicate到客户端
 	// 此时Ability是预测的，不会受这个影响，会生成预测火球
@@ -28,7 +28,7 @@ void UMy_AuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLoc
 	IMy_CombatInterface* CombatInterface = Cast<IMy_CombatInterface>(GetAvatarActorFromActorInfo());
 	if (CombatInterface)
 	{
-		const FVector SockLoc = IMy_CombatInterface::Execute_GetWeaponSockLocation(GetAvatarActorFromActorInfo(),FMy_AuraGameplayTags::GetInstance().My_CombatSocket_Weapon);
+		const FVector SockLoc = IMy_CombatInterface::Execute_GetWeaponSockLocation(GetAvatarActorFromActorInfo(),SocketTag);
 		FRotator Rotation = (ProjectileTargetLocation - SockLoc).Rotation();
 
 		FTransform SpawnTransform;
--- Source/Aura/Public/MY_AbilitySystem/Ability/My_AuraProjectileSpell.h
+++ Source/Aura/Public/MY_AbilitySystem/Ability/My_AuraProjectileSpell.h
@@ -20,7 +20,7 @@ protected:
 	virtual void  ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
 	UFUNCTION(BlueprintCallable, Category="My_Projectile")
-	void SpawnProjectile(const FVector& ProjectileTargetLocation);
+	void SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag);
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<AMy_ProjectileActor> ProjectileClass;
